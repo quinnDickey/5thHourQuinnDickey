4f3f75d971e45fc2a6357e5b7b1ba9b1297040aa
ithubquinnDickey <qd125016@student.wpsok.org>
2024-10-09 12:58:39 -0500

    added hw

--- HW5.py
+++ HW5.py
@@ -6,12 +6,12 @@
 print("hello world")
 
 numbList= [23,12,76]
-if numbList[0]<numbList[1]<numbList[2]:
+
+if numbList[0]<numbList[1]<numbList[2] or numbList[1]<numbList[0]<numbList[2]:
      print("3th number greatest number"),
-if numbList[1]<numbList[2]<numbList[0]:
-    print("1st number is the greatest number"),
-if numbList[2]<numbList[0]<numbList[1]:
+elif numbList[2]<numbList[1]<numbList[0] or numbList[2]<numbList[3]<numbList[2]:
+    print("1th number greatest number"),
+elif numbList[2]<numbList[0]<numbList[1]:
     print("2st number is the greatest number"),
 else:
       print("")
-
