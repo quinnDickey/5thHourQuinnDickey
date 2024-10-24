e131f75247b80a4fcfd3d748668ffecf74d8118a
ithubquinnDickey <qd125016@student.wpsok.org>
2024-10-24 13:16:03 -0500

    hw9

---
+++ hw9.py
@@ -0,0 +1,16 @@
+#Name:Quinn Dickey
+#Class: 5th Hour
+#Assignment: HW9
+
+print("hello world")
+
+temp=int(input("What is the temperature outside?"))
+
+if temp > 20:
+    print("Its hot"),
+elif temp > 10:
+    print("its mild"),
+else:
+    print("its cold"),
+
+print("thank you")
