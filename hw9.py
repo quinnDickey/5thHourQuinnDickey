126c6fad8b41d063611c6b3a250f1a6de47b7b5e
ithubquinnDickey <qd125016@student.wpsok.org>
2024-11-05 12:59:13 -0600

    hw9 update

--- hw9.py
+++ hw9.py
@@ -2,7 +2,12 @@
 #Class: 5th Hour
 #Assignment: HW9
 
+#1. Print Hello World!
+
 print("hello world")
+
+
+#2. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
 
 temp=int(input("What is the temperature outside?"))
 
