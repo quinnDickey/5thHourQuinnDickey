81493c7fc4a27966d6331053ee2fefff3a51c2c6
ithubquinnDickey <qd125016@student.wpsok.org>
2025-02-20 13:09:38 -0600

    hw19

---
+++ hw19.py
@@ -0,0 +1,14 @@
+#Name: Quinn Dickey
+#Class: 5th Hour
+#Assignment: HW19#1.
+
+#1. Import all the functions from the HW15 file.
+
+from HW15 import hello_world, average, animals_list, count
+
+
+#2. Call the functions here and run HW19.
+
+
+#3. Delete all calls from HW15 and run HW19 again.
+
